e6bf053d3c0e51d67219177fb2f725877bb66d28
Tim Ubial <tubial@sd38.bc.ca>
2019-06-03 09:21:53 -0700

    Download is functional.

--- multidownloadXkcd.py
+++ multidownloadXkcd.py
@@ -1,7 +1,10 @@
 # multidownloadXkcd.py
 # Downloads XKCD comics using multipled threads
 
-import requests, os, bs4, threading
+import requests
+import os
+import bs4
+import threading
 
 # create or open a dir ./xkcd
 os.makedirs('xkcd', exist_ok=True)
@@ -16,10 +19,41 @@ def downloadXkcd(start_comic, end_comic):
     """
 
     for url_number in range(start_comic, end_comic + 1):
-        pass
-        # TODO: Download the page
-        # TODO: Find the URL of the comic
-        # TODO: Save the image if it exists
+        print(f'Downloading the page http://xkcd.com/{url_number}...')
+        res = requests.get(f'http://xkcd.com/{url_number}')
+        res.raise_for_status()
+
+        # Find the URL of the comic
+        soup = bs4.BeautifulSoup(res.content, 'html.parser')
+
+        comic_elem = soup.select('#comic img')
+
+
+        # Save the image if it exists
+        if comic_elem == []:
+            print('Could not find comic.')
+        else:
+            comic_url = 'http:' + comic_elem[0].get('src')
+
+            # Download the image
+            print(f'Downloading image {url_number}...')
+            res = requests.get(comic_url)
+            res.raise_for_status()
+
+            # Save the image
+            image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
+            image_file.close()
+
+
+def main():
+    downloadXkcd(1, 5)
+
+
+if __name__ == "__main__":
+    main()
+
 
 # TODO: Create and start the Threads
 
